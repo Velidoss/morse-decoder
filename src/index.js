aba9a6f8c4ae74099d7558038f9cd6a3122bcd6f
Velidoss <velidoss11@gmail.com>
2020-10-07 11:15:58 +0300

    task solved

--- src/index.js
+++ src/index.js
@@ -38,7 +38,38 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = []
+    for(let  i = 0; i< expr.length; i+=10){
+        arr.push(expr.slice(i, i+10))
+    }
+    for(let i = 0; i< arr.length; i++){
+        let newEl = []
+        for(let j = 0; j<arr[i].length; j+=2){
+            newEl.push(arr[i].slice(j, j+2))
+        }
+        arr[i] = newEl
+        arr[i] = arr[i].reduce((acc, el)=>{
+            if(el === '00'){
+                return acc+=''
+            }else if(el ==='10'){
+                return acc+='.'
+            }else if(el ==='11'){
+                return acc+='-'
+            }else if(el ==='**'){
+                return acc+='*'
+            }
+        }, '')
+        for(let j=0; j<Object.entries(MORSE_TABLE).length; j++){
+            if(Object.entries(MORSE_TABLE)[j][0] === arr[i]){
+                arr[i] = Object.entries(MORSE_TABLE)[j][1]
+            }
+        }
+        if(arr[i]==="*****"){
+            arr[i] = ' '
+        }
+    }
+
+    return arr.join('')
 }
 
 module.exports = {
